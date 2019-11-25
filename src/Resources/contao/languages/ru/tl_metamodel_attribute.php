2a94d082237bc9138a2c71981cd840253f8325e9
Mini Model <minimodel@metamodel.me>
2019-11-25 21:15:46 +0100

    Updated translations from transifex.

--- src/Resources/contao/languages/ru/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/ru/tl_metamodel_attribute.php
@@ -4,9 +4,10 @@
  */
 
 
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_cols']['0']     = 'Конфигурация колонки';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowLabel']['0'] = 'Метка';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowLabel']['1'] = 'Введите метки для колонок';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowStyle']['0'] = 'Ширина';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowStyle']['1'] = 'Введите ширину колонок (наприм., 200px)';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_cols']['0']      = 'Конфигурация колонки';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowLabel']['0']  = 'Метка';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowLabel']['1']  = 'Введите метки для колонок';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowStyle']['0']  = 'Ширина';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowStyle']['1']  = 'Введите ширину колонок (наприм., 200px)';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tablemulti'] = 'Множественный таблицы';
 
