f297ff29e0dfd5c55cb6496622c81c2d3088d32d
Mini Model <minimodel@metamodel.me>
2023-02-01 17:27:49 +0100

    Updated translations from transifex.

--- src/Resources/contao/languages/de/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/de/tl_metamodel_attribute.php
@@ -4,10 +4,5 @@
  */
 
 
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_cols']['0']      = 'Spalteneinstellung';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowLabel']['0']  = 'Label';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowLabel']['1']  = 'Bitte geben Sie die Labels für die Spalten an.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowStyle']['0']  = 'Breite';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tablemulti_rowStyle']['1']  = 'Bitte geben Sie die Spaltenbreite ein (beispielsweise 200px).';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tablemulti'] = 'Tabelle multi (MCW)';
 
