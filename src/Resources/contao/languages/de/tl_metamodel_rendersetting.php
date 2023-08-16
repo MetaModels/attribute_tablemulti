8b09ff6aba59d44d07b098e430548882cae0ed37
Mini Model <minimodel@metamodel.me>
2023-08-16 21:01:42 +0200

    Updated translations from transifex.

--- src/Resources/contao/languages/de/tl_metamodel_rendersetting.php
+++ src/Resources/contao/languages/de/tl_metamodel_rendersetting.php
@@ -1,9 +1,25 @@
 <?php
+
 /**
-
+ * This file is part of MetaModels/attribute_tablemulti.
+ *
+ * (c) 2012-2023 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * Last-updated: 2023-08-16T21:01:42+02:00
+ *
+ * @copyright 2012-2023 The MetaModels team.
+ * @license   https://github.com/MetaModels/attribute_tablemulti/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
-
 
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['tablemulti_hide_tablehead']['0'] = 'Tabellenkopf verbergen';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['tablemulti_hide_tablehead']['1'] = 'Auswählen, um den Tabellenkopf zu verbergen.';
-
